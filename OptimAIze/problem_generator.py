98835d40f83510b4cf17c3b7c1f3040412515d24
Kragon-Nox <55194205+Kragon-Nox@users.noreply.github.com>
2020-06-09 11:02:39 +0200

    Update problem_generator.py
    Légères modifications import

--- OptimAIze/problem_generator.py
+++ OptimAIze/problem_generator.py
@@ -6,8 +6,8 @@
 # For more information see its description at the end of the code
 
 import numpy as np
-from optimAIze.dataset import dataset
-from optimAIze.problem_selector import Selector, extract
+from OptimAIze.dataset import dataset
+from OptimAIze.problem_selector import Selector, extract
 
 # lin_opt_pbs is a class representing linear optimization problems.
 # It will be used to generate new linear optimization problems
