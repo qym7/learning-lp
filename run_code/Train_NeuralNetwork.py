Fixed vertex computation

run_code/Train_NeuralNetwork.py
@@ -31,20 +31,20 @@ if __name__ == '__main__':
         """Setting training parameters"""
 
         depth = 1
-        layers = [500 for i in range(depth)]
-        epochs = 50
+        layers = [2000 for i in range(depth)]
+        epochs = 80
         validation_split = 0.3
 
         """Setting callbacks"""
 
-        callbacks = [tf.keras.callbacks.LearningRateScheduler(scheduler_LandS_opt_on_model_1_500_1),
+        callbacks = [tf.keras.callbacks.LearningRateScheduler(scheduler_20term_opt_on_model_1_500_1),
                      tf.keras.callbacks.ModelCheckpoint(filepath="D:\\repository\\learning-lp\\data\\Model_checkpoints",
                                                         save_weights_only=True, verbose=0)]
         #callback = EscapeLocalMinima(scheduler_LandS_opt_on_model_1_100_1, considered_epochs=3, threshold=4e-3)
 
         """Creating neural network."""
 
-        network = NeuralNetwork(file_name="LandS_network_1_500_1_test")
+        network = NeuralNetwork(file_name="20term_network_1_2000_1")
         network.basic_nn(layers)
         network.add_bound_processors([BoundProcessorNormalise()])
         network.add_solution_processors([SolutionProcessorLinearMax()])
@@ -144,12 +144,9 @@ if __name__ == '__main__':
         network.graph_save("LandS_network_1_500_1_5e-4", path="D:\\repository\\learning-lp\\data\\Trained_networks")
 
     if False:
-        network = graph_load("LandS_network_1_500_1_test_layers.model", "LandS_network_1_500_1_test_activations.model",
-                             "LandS_network_1_500_1_test_processing.model",
+        network = graph_load("LandS_network_1_500_1_layers_3e-6.model", "LandS_network_1_500_1_activations_3e-6.model",
+                             "LandS_network_1_500_1_processing_3e-6.model",
                              path="D:\\repository\\learning-lp\\data\\Trained_networks")
-
-        network.input_names = ["S2C5", "S2C6", "S2C7", "X1", "X2", "X3", "X4"]
-        #network = load_model("gbd_network_1_500_1_3e-5", path="D:\\repository\\learning-lp\\data\\Trained_networks")
 
         path = sys.argv[1]
 
@@ -160,6 +157,5 @@ if __name__ == '__main__':
 
         print(OutputDataAnalyser(Output).mean_precision_error())
 
-        network.graph_save(name="LandS_network_1_500_1_test_copy", path="D:\\repository\\learning-lp\\data\\Trained_networks")
 
 

src_python/NeuralNetwork.py
@@ -526,6 +526,11 @@ class NeuralNetwork:
                     file_processing.write(str(elem) + (7 * " "))
                 file_processing.write("\n")
 
+            if isinstance(processor, BoundProcessorApplyFunction):
+                if processor.function == np.log10:
+                    file_processing.write("Input-processing" + (7 * " ") + "Logarithmic" + (9 * " ") +
+                                          str(processor.number))
+
         if add_bias_number_first_layer > 0:
             file_processing.write("Input-processing" + (7 * " ") + "Additional_bias" + (5 * " ")
                                   + str(add_bias_number_first_layer) + "\n")
@@ -768,7 +773,7 @@ def graph_load(file_layers, file_activations, file_processing, path=None):
     processors = file_p.readlines()
     nb_proc = len(processors)
     processor = None
-    boundprocesors = []
+    boundprocessors = []
     solutionprocessors = []
 
     for i in range(nb_proc):
@@ -785,13 +790,17 @@ def graph_load(file_layers, file_activations, file_processing, path=None):
                 processor = BoundProcessorNormalise(mean_list, dev_list)
             else:
                 if line[0] == "Input-processing":
-                    boundprocesors.append(processor)
+                    boundprocessors.append(processor)
                 else:
                     solutionprocessors.append(processor)
 
         if line[1] == "Additional_bias":
             processor = BoundProcessorAddConst(number_of_const=int(line[2]))
-            boundprocesors.append(processor)
+            boundprocessors.append(processor)
+
+        if line[1] == "Logarithmic":
+            processor = BoundProcessorApplyFunction(np.log10, int(line[2]))
+            boundprocessors.append(processor)
 
         if line[1] == "Multiply_by":
             processor = SolutionProcessorLinearMax()
@@ -813,7 +822,7 @@ def graph_load(file_layers, file_activations, file_processing, path=None):
     for i in range(nb_layers):
         layers[i].set_weights(network[i])
 
-    neural_network.add_bound_processors(boundprocesors)
+    neural_network.add_bound_processors(boundprocessors)
     neural_network.add_solution_processors(solutionprocessors)
     neural_network.activate_processors()
 

src_python/Problem_xpress.py
@@ -52,19 +52,31 @@ class Xpress_problem(Problem):
         """
         nb_rows = self.content.attributes.rows
         nb_cols = self.content.attributes.cols
+        nb_e = 0
 
         names = self.get_variable_names()
 
         indices = []
         values = []
+        row_types = []
+        self.content.getrowtype(row_types, 0, nb_rows - 1)
 
-        matrix = nb_rows * [None]
+        for elem in row_types:
+            if elem == 'E':
+                nb_e += 1
+
+        matrix = (nb_rows + nb_e) * [None]
+        rhs = (nb_rows + nb_e) * [None]
+
+        index = 0
 
         for i in range(nb_rows):
-            row = nb_cols * [None]
+            row = np.array(nb_cols * [None])
             counter = 0
             self.content.getrows(mstart=None, mclind=indices, dmatval=values, size=nb_cols, first=i, last=i)
             non_zeros = len(values)
+            elem = []
+            self.content.getrhs(elem, i, i)
 
             for j in range(nb_cols):
                 if counter < non_zeros and names[j] == indices[counter].name.strip():
@@ -72,9 +84,23 @@ class Xpress_problem(Problem):
                     counter += 1
                 else:
                     row[j] = 0
-            matrix[i] = row
 
-        return np.array(matrix)
+            if row_types[i] == 'L':
+                matrix[index] = row
+                rhs[index] = elem[0]
+                index += 1
+            elif row_types[i] == 'G':
+                matrix[index] = -row
+                rhs[index] = -elem[0]
+                index += 1
+            else:
+                matrix[index] = row
+                matrix[index+1] = -row
+                rhs[index] = elem[0]
+                rhs[index+1] = -elem[0]
+                index += 2
+
+        return np.array(matrix), np.array(rhs)
 
     def set_RHS(self, rhs):
         """

src_python/problem_selector.py
@@ -66,7 +66,7 @@ class Selector:
     def fill_content(self):
         """
         The method fill_content converts the content of the linear optimization problems in
-        self.prob_list to the format required by the class lin_opt_pbs ans saves it in self.content.
+        self.prob_list to the format required by the class lin_opt_pbs and saves it in self.content.
 
         More precisely the method checks first of all if the content of prob_list is a string list.
         If that is not the case, an error message will occur. If the content of prob_list is a string
@@ -138,16 +138,17 @@ class Selector:
 
     def calculate_vertices(self, problem):
         if self.vertices is None:
-            matrix = problem.get_matrix()
+            matrix, rhs = problem.get_matrix()
             index = len(matrix[0])
 
             identity = - np.identity(index)
             zeros = np.zeros(index)
 
             matrix = np.concatenate((matrix, identity))
-            rhs = np.concatenate((np.array(problem.get_RHS(all_cons=True)), zeros))
+            rhs = np.concatenate((rhs, zeros))
 
             vertices = pypoman.compute_polytope_vertices(matrix, rhs)
+
             return vertices
 
     def read_cons_to_vary(self, problem):